9fa6b19c44a2be29daebf71becb3a46fd1ea1f97
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-02-18 21:29:37 +0000

    Cucumber junit

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,24 @@
+package com.cydeo.step_definitions;
+
+
+/*
+In the class we will be able to pass pre- & post- conditions to each scenario and each step
+ */
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    //import from io.cucumber.java not from junit
+    @Before
+    public void setupScenario(){
+        System.out.println("====Setting up browser using cucumber @Before");
+    }
+
+    @After
+    public void teardowndScenario(){
+        System.out.println("====Closing browser using cucumber @After");
+    }
+
+}
