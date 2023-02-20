2346720936a50836747e105e647a153f13914bb5
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-02-20 17:09:31 +0000

    Day 16 Hooks.java take screen shot

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -5,10 +5,7 @@ In the class we will be able to pass pre- & post- conditions to each scenario an
  */
 
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -32,9 +29,17 @@ public class Hooks {
 
 
     @After
-    public void teardowndScenario(){
+    public void teardowndScenario(Scenario scenario){
 
-        byte [] screenshot= ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()){
+
+            byte [] screenshot= ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png", scenario.getName());
+
+        }
+
+
+
 
         Driver.closeDriver();
 
