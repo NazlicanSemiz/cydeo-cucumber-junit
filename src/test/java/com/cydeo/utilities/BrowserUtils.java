2f6576cc32e69d9ba4ce132e3c9165d186d9b0e1
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-02-22 14:16:01 +0000

    day17 DropdownsPage.java

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,26 +1,37 @@
 package com.cydeo.utilities;
 
-import org.junit.Assert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.cydeo.utilities.Driver;
+import org.junit.*;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.*;
+import org.openqa.selenium.support.ui.*;
 
-import java.util.Set;
+import java.util.*;
 
 public class BrowserUtils {
 
-    public static void sleep(int second) {
+    /*
+This method will accept int (in seconds) and execute Thread.sleep
+for given duration
+ */
+    public static void sleep(int second){
         second *=1000;
         try {
             Thread.sleep(second);
-        }catch (InterruptedException e){
-
+        }catch (InterruptedException e ) {
 
         }
     }
 
-    public static void switchWindowAndVerify( String expectedInUrl, String expectedInTitle){
+    /*
+    This method accepts 3 arguments.
+    Arg1: webdriver
+    Arg2: expectedInUrl : for verify if the url contains given String.
+        - If condition matches, will break loop.
+    Arg3: expectedInTitle to be compared against actualTitle
+     */
+    public static void switchWindowAndVerify(String expectedInUrl, String expectedInTitle){
 
         Set<String> allWindowsHandles = Driver.getDriver().getWindowHandles();
 
@@ -32,39 +43,51 @@ public class BrowserUtils {
 
             if (Driver.getDriver().getCurrentUrl().contains(expectedInUrl)){
                 break;
-
             }
         }
 
-
-        //5. Assert: Title contains “Etsy”
+        //5. Assert:Title contains “expectedInTitle”
         String actualTitle = Driver.getDriver().getTitle();
-
-
         Assert.assertTrue(actualTitle.contains(expectedInTitle));
-
-
-
     }
 
-/*
-This method accepts String "expectedTtitle" and Assert if its true
- */
-
+    /*
+    This method accepts a String "expectedTitle" and Asserts if it is true
+     */
     public static void verifyTitle(String expectedTitle){
 
         Assert.assertEquals(Driver.getDriver().getTitle(), expectedTitle);
 
     }
 
-    /*
-    Creating a utility method for ExplicitWait, so we don't have to repeat the lines
+    /**
+     * This method will accept a String as expected value and verify actual URL CONTAINS the value.
+     * @param expectedInURL
      */
-    public static void waitForInvisibilityOf(WebElement webElement){
-        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
-        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    public static void verifyURLContains(String expectedInURL){
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedInURL));
     }
 
 
-}
+    /**
+     * This method will accept a dropdown as a WebElement
+     * and return all the options' text in a List of String.
+     * @param dropdownElement
+     * @return List<String> actualOptionsAsString
+     */
+    public static List<String> dropdownOptionsAsString(WebElement dropdownElement) {
+        Select select = new Select(dropdownElement);
+
+        //List of all ACTUAL month <options> as a web element
+        List<WebElement> actualOptionsAsWebElement = select.getOptions();
+
+        //List of all ACTUAL month <options> as a string
+        List<String> actualOptionsAsString = new ArrayList<>();
+
+        for (WebElement each : actualOptionsAsWebElement) {
+            actualOptionsAsString.add(each.getText());
+        }
+
+        return actualOptionsAsString;
+
+    }}
